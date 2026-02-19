a3a92b3069508d4b4609ce9d4ea936c001c3bd83
Kaleb McKinney <kalebmckinney@outlook.com>
2026-02-19 11:56:56 -0500

    wireframe UI with some dummy entries to show how the UI will work

--- FrienDex/RolodexPage.xaml.cs
+++ FrienDex/RolodexPage.xaml.cs
@@ -1,3 +1,8 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
 namespace FrienDex;
 
 public partial class RolodexPage : ContentPage
@@ -5,5 +10,81 @@ public partial class RolodexPage : ContentPage
 	public RolodexPage()
 	{
 		InitializeComponent();
+		BindingContext = new RolodexPageViewModel(this);
 	}
+}
+
+public class RolodexPageViewModel : INotifyPropertyChanged
+{
+	private readonly Page _page;
+	private ObservableCollection<Contact> contacts = new();
+
+	public ObservableCollection<Contact> Contacts
+	{
+		get => contacts;
+		set
+		{
+			if (contacts != value)
+			{
+				contacts = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public ICommand AddContactCommand { get; }
+	public ICommand ContactSelectedCommand { get; }
+
+	public RolodexPageViewModel(Page page)
+	{
+		_page = page;
+		Contacts = new ObservableCollection<Contact>();
+		AddContactCommand = new Command(OnAddContact);
+		ContactSelectedCommand = new Command<Contact>(OnContactSelected);
+
+		// TODO: Load contacts from your data source
+		LoadContacts();
+	}
+
+	private void LoadContacts()
+	{
+		// Replace with actual data loading
+		Contacts.Add(new Contact { Name = "John Doe", Room = "Study Group" });
+		Contacts.Add(new Contact { Name = "Jane Smith", Room = "Gaming" });
+		Contacts.Add(new Contact { Name = "Bob Johnson", Room = null });
+	}
+
+	private void OnAddContact()
+	{
+		// TODO: Navigate to add contact page or show dialog
+		MainThread.BeginInvokeOnMainThread(async () =>
+		{
+			await _page.DisplayAlertAsync("Add Contact", "Add contact functionality coming soon", "OK");
+		});
+	}
+
+	private void OnContactSelected(Contact contact)
+	{
+		// TODO: Navigate to contact details
+		if (contact != null)
+		{
+			MainThread.BeginInvokeOnMainThread(async () =>
+			{
+				await _page.DisplayAlertAsync("Contact Selected", $"Selected: {contact.Name}", "OK");
+			});
+		}
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
+
+public class Contact
+{
+	public string Name { get; set; } = string.Empty;
+	public string? Room { get; set; }
 }
--- FrienDex/RoomsPage.xaml.cs
+++ FrienDex/RoomsPage.xaml.cs
@@ -1,3 +1,8 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
 namespace FrienDex;
 
 public partial class RoomsPage : ContentPage
@@ -5,5 +10,82 @@ public partial class RoomsPage : ContentPage
 	public RoomsPage()
 	{
 		InitializeComponent();
+		BindingContext = new RoomsPageViewModel(this);
 	}
+}
+
+public class RoomsPageViewModel : INotifyPropertyChanged
+{
+	private readonly Page _page;
+	private ObservableCollection<Room> rooms = new();
+
+	public ObservableCollection<Room> Rooms
+	{
+		get => rooms;
+		set
+		{
+			if (rooms != value)
+			{
+				rooms = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public ICommand AddRoomCommand { get; }
+	public ICommand RoomSelectedCommand { get; }
+
+	public RoomsPageViewModel(Page page)
+	{
+		_page = page;
+		Rooms = new ObservableCollection<Room>();
+		AddRoomCommand = new Command(OnAddRoom);
+		RoomSelectedCommand = new Command<Room>(OnRoomSelected);
+
+		// TODO: Load rooms from your data source
+		LoadRooms();
+	}
+    /// <summary>
+    /// Fetches the list of rooms from the data source and populates the Rooms collection.
+    /// </summary>
+    private void LoadRooms()
+	{
+		// Replace with actual data loading
+		Rooms.Add(new Room { Name = "Study Group", Description = "Computer Science" });
+		Rooms.Add(new Room { Name = "Gaming", Description = "Multiplayer games" });
+	}
+
+	private void OnAddRoom()
+	{
+		// TODO: Navigate to add room page or show dialog
+		MainThread.BeginInvokeOnMainThread(async () =>
+		{
+			await _page.DisplayAlertAsync("Add Room", "Add room functionality coming soon", "OK");
+		});
+	}
+
+	private void OnRoomSelected(Room room)
+	{
+		// TODO: Navigate to room details
+		if (room != null)
+		{
+			MainThread.BeginInvokeOnMainThread(async () =>
+			{
+				await _page.DisplayAlertAsync("Room Selected", $"Selected: {room.Name}", "OK");
+			});
+		}
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
+
+public class Room
+{
+	public string Name { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
 }
