1e2a94c545d190be05388ae71223bfa81408b17f
Quinn D. <danielss1@etsu.edu>
2026-03-05 14:39:59 -0500

    Bringing in the CreateWithBatch and sync additions
    wanted to get at least these pushed somewhere.
    
    currently fighting for my life with dependency injection for the initializer service

---
+++ FrienDex/Services/PersonRepo.Syncronous.cs
@@ -0,0 +1,64 @@
+﻿using FrienDex.Data;
+using FrienDex.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrienDex.Services
+{
+    public partial class PersonRepo : IPersonRepo
+    {
+        public Person CreateSyncronous(Person newPerson)
+        {
+            _db.People.Add(newPerson);
+            _db.SaveChanges();
+            return newPerson;
+        }
+
+        public IEnumerable<Person> CreateWithListSyncronous(List<Person> newPeople)
+        {
+            var returnedPeople = new List<Person>();
+            foreach (var person in newPeople)
+            {
+                _db.People.Add(person);
+                returnedPeople.Add(person);
+                try
+                {
+                    _db.SaveChanges();
+                    _logger.LogInformation("Person created & saved in the Database using batch method.");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(
+                        $"\n\n---------------- CreatePerson (batch) --- log ---------\n\n"
+                        + $"An error occurred while adding lists to the database. And saving a person to the DB {ex.Message}"
+                        //        +$"\n\n---------------- SEED DATA ASYNC --- log ---------\n\n");
+                        + "\n\n\t{FirstName} {LastName}\n\n"
+                        //    Console.WriteLine(
+                        + $"\n\n----- 1 -------- CreatePerson (batch) --- console ----\n\n"
+                        //        + $"An error occurred while adding lists to the database. {ex.Message}"
+                        //        + $"\n\n----- 2 -------- SEED DATA ASYNC --- console ----\n\n"
+                        + $"{ex}"
+                        + $"\n\n---- end ------- CreatePerson (batch) --- console ----\n\n"
+                        , person.FirstName, person.LastName);
+                }
+            }
+            return returnedPeople;
+        }
+
+        public Person? ReadSyncronous(int id)
+        {
+            return _db.People.Find(id);
+        }
+
+        public ICollection<Person> ReadAllSyncronous()
+        {
+            return _db.People.ToList();
+        }
+
+
+
+    }
+}
--- FrienDex/Services/PersonRepo.cs
+++ FrienDex/Services/PersonRepo.cs
@@ -1,19 +1,26 @@
 ﻿using FrienDex.Data;
 using FrienDex.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FrienDex.Services
 {
-    public class PersonRepo : IPersonRepo
+    public partial class PersonRepo : IPersonRepo
     {
         private readonly DexContext _db;
+        private readonly ILogger<Person> _logger;
 
-        public PersonRepo(DexContext db)
+
+        public PersonRepo(
+            DexContext db,
+            ILogger<Person> logger
+            )
         {
             _db = db;
+            _logger = logger;
         }
 
         public async Task<Person> CreateAsync(Person newPerson)
@@ -21,6 +28,48 @@ namespace FrienDex.Services
             await _db.People.AddAsync(newPerson);
             await _db.SaveChangesAsync();
             return newPerson;
+        }
+        public async Task<IEnumerable<Person>> CreateWithListAsync(List<Person> newPeople)
+        {
+            // Create a list to people to return
+            var returnedPeople = new List<Person>();
+
+            // in case we have to modify to add multiple entities at once
+            // Zip allows iteration of multiple arrays at once. If arrays are different lengths, it will evaluate to the length of the shortest array
+            // source: https://stackoverflow.com/a/41534351
+            // NOTE: was applicable when creating a multi-user app (thx AspNetCore.Identity :/ )
+            //          but may not be applicable here. Feel free to remove this comment if you think it's unnecessary
+
+            foreach (var person in newPeople)
+            {
+                await _db.People.AddAsync(person);
+                returnedPeople.Add(person);
+                try
+                {
+                    await _db.SaveChangesAsync();
+                    _logger.LogInformation("Person created & saved in the Database using batch method.");
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(
+                        $"\n\n---------------- CreateUserDexHolder (batch) --- log ---------\n\n"
+                        + $"An error occurred while adding lists to the database. And saving a person to the DB {ex.Message}"
+                        //        +$"\n\n---------------- SEED DATA ASYNC --- log ---------\n\n");
+                        
+                        + "\n\n\t{FirstName} {LastName}\n\n"
+                        //    Console.WriteLine(
+                        + $"\n\n----- 1 -------- CreateUserDexHolder (batch) --- console ----\n\n"
+                        //        + $"An error occurred while adding lists to the database. {ex.Message}"
+                        //        + $"\n\n----- 2 -------- SEED DATA ASYNC --- console ----\n\n"
+                        + $"{ex}"
+                        + $"\n\n---- end ------- CreateUserDexHolder (batch) --- console ----\n\n"
+                        , person.FirstName, person.LastName);
+
+                }
+            }
+
+            return returnedPeople;
         }
 
         public async Task<ICollection<Person>> ReadAllAsync()
